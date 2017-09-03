8dc75db8c1d48ba695d839c61da8a876842d7d63
ac <ac@acdeMacBook-Pro.local>
2017-09-03 22:46:52 +0800

    1

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -15,13 +15,19 @@ export default {
         this.line(ctx,20,20,20,column.height - 30)
         this.line(ctx,20,column.height - 30,column.width,column.height - 30);
         var arr = [20,40,10,100,50,80,230];
+        var max = Math.max.apply(null,arr);
         [1,2,3,4,5,6,7].forEach(function (item,i){
             ctx.beginPath();
-            ctx.moveTo(60 + i * (column.width - 60) / 7,column.height - 45);
-            ctx.lineTo(60 + i * (column.width - 60) / 7,(column.height - 30) * (arr[i] / 500));
+            ctx.moveTo(60 + i * (column.width - 60) / 7,column.height - 60);
+            ctx.lineTo(60 + i * (column.width - 60) / 7,column.height - 30  - ((column.height - 50) * (arr[i] / max)));
+            console.log(60 + i * (column.width - 60) / 7,column.height - 60,'moveTo')
+            console.log(60 + i * (column.width - 60) / 7,column.height - 100  - ((column.height - 50) * (arr[i] / max)))
             ctx.lineWidth = 20;
             ctx.lineCap = 'round';
-            ctx.strokeStyle = 'orange'
+            var grd = ctx.createLinearGradient(60 + i * (column.width - 60) / 7,column.height - 45,60 + i * (column.width - 60) / 7,column.height - 50  - ((column.height - 50) * (arr[i] / 500))); //grd定义为描边渐变样式;
+            grd.addColorStop(0, 'orange');
+            grd.addColorStop(1, 'blue');
+            ctx.strokeStyle = grd;
             ctx.stroke();
             self.write(ctx,(i+1),'block',60 + i * (column.width - 60) / 7, column.height - 10)
         })
